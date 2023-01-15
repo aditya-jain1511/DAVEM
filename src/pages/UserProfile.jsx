677a9952f3cf04722c9d9e73e506216c5739caef
Aditya Jain <aditya151101@gmail.com>
2023-01-16 02:26:45 +0530

    user detail + footer

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,13 +1,64 @@
 import React from 'react'
 import { useContext } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Table } from 'reactstrap';
 import { LoginContext } from '../context/LoginContext';
+import { UserContext } from '../context/UserContext';
 
 export default function UserProfile() {
+    const {user, setUser} = useContext(UserContext);
     const {login, setLogin} = useContext(LoginContext);
 
-
     return (
-        <Button color="dark" outline onClick = {()=> setLogin(false)}>Logout</Button>
+        <div className="jumbotron">
+            <div className="container">
+                <h2>User Details:</h2>
+                <div className="row">
+                    <div className="col-6">
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">UserId:</div>
+                            <div className="col-12 col-md">{user.userId}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">UserName:</div>
+                            <div className="col-12 col-md">{user.username}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">Name:</div>
+                            <div className="col-12 col-md">{user.name}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">User Type:</div>
+                            <div className="col-12 col-md">{user.userType}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">Phone No:</div>
+                            <div className="col-12 col-md">{user.phone}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">Email:</div>
+                            <div className="col-12 col-md">{user.email}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">Gender:</div>
+                            <div className="col-12 col-md">{user.gender}</div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-4">University RollNo:</div>
+                            <div className="col-12 col-md">{user.rollno}</div>
+                        </div>
+                        <hr />
+                        <Button color="dark" outline size='sm' onClick = {()=> setLogin(false)}>Logout</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/sectors/Footer.jsx
+++ src/sectors/Footer.jsx
@@ -1,7 +1,110 @@
-import React from 'react'
+import React from "react";
+import "../css/footer.css";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaEnvelope,
+  FaPhoneAlt,
+} from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import { Link } from "react-router-dom";
+import { NavLink } from "reactstrap";
 
-export default function Footer() {
+function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+
+    <div className="container">
+      <div className="row align-items-start" style={{ textAlign: "left" }}>
+        <div className="col-12 col-lg-3">
+        <img src="assets/images/daviet-logo.png" alt="Daviet Logo" className="footer-img"/>
+        <span className="footer-logo">DAVEM</span>
+          <div style={{ marginBottom: "20px" }}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam
+          </div>
+          <div>
+            <span>
+              <FaFacebookF className="faicons" />
+            </span>
+            <span>
+              <FaInstagram className="faicons" />
+            </span>
+            <span>
+              <FaTwitter className="faicons" />
+            </span>
+            <span>
+              <FaLinkedin className="faicons" />
+            </span>
+            <span>
+              <FaGithub className="faicons" />
+            </span>
+          </div>
+        </div>
+        <div className="col-12 col-lg-2 offset-lg-3">
+          <div className="footerhead">Corner</div>
+          <div className="d-none d-lg-block">
+            <div className="footercont">Academics</div>
+            <div className="footercont">Students</div>
+            <div className="footercont">Admission</div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-2">
+          <div className="footerhead">Links</div>
+          <div className="d-none d-lg-block">
+            <div className="footercont" ><Link className="footercont" to="/home"> Home</Link></div>
+            <div className="footercont"><Link className="footercont" to="/events"> Events</Link></div>
+            <div className="footercont"><Link className="footercont" to="/contacts"> Contact Us</Link></div>
+            <NavLink className="footercont" href="https://davietjal.org" target="_blank" rel="noopener noreferrer">
+                DAVIET
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-12 col-lg-2">
+          <div className="footerhead">Contact us</div>
+          <div className="d-none d-lg-block">
+            <div className="footercont">
+              <FaEnvelope /> daviet@davietjal.org
+            </div>
+            <div className="footercont">
+              <FaPhoneAlt /> +91-1812343400/01
+            </div>
+            <div className="footercont">
+              <FaPhoneAlt /> TollFree- 18601800126
+            </div>
+            <div className="footercont">
+              <GoLocation /> Kabir Nagar, Opposite Bulton Park, Jalandhar -144008
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="row align-items-center bottomfoot">
+        <div
+          className="col-12"
+          style={{ textAlign: "center", marginBottom: "50px" }}
+          >
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry. Lorem Ipsum has been
+          the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book.{" "}
+        </div>
+        <div className="col-12 col-lg-6">
+          Copyright © 2022 DAV Institute of Engineering and Technology
+        </div>
+        <div className="col-12 col-lg-6 ms-auto" style={{ textAlign: "right" }}>
+          All Rights Reserved | Terms and Conditions | Privacy Policy
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
+
+export default Footer;
