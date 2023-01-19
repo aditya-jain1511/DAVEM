277120b7d109fb32e653945b92d03207ac984eaf
Aditya Jain <aditya151101@gmail.com>
2023-01-19 18:35:09 +0530

    Dummy Data

---
+++ src/shared/department.js
@@ -0,0 +1,27 @@
+export const DEPARTMENTS = [
+    {
+        id: 0,
+        departId: 'IT',
+        hod: 'TEACH000'
+    },
+    {
+        id: 1,
+        departId: 'CSE',
+        hod: 'TEACH001'
+    },
+    {
+        id: 2,
+        departId: 'ECE',
+        hod: 'TEACH002'
+    },
+    {
+        id:4,
+        departId: 'CULTURE',
+        hod: 'TEACH003'
+    },
+    {
+        id:5,
+        departId: 'SPORTS',
+        hod: 'TEACH004'
+    }
+];
---
+++ src/shared/venue.js
@@ -0,0 +1,17 @@
+export const VENUES = [
+    {
+        id: 0,
+        venueId: 'VENUE000',
+        location: 'Sports Ground'
+    },
+    {
+        id: 1,
+        venueId: 'VENUE001',
+        location: 'Auditorium'
+    },
+    {
+        id: 2,
+        venueId: 'VENUE002',
+        location: 'C-Hall'
+    }
+];
